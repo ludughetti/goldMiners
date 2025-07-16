3907148b44cbec3b2d021d9afd0aeb7137646bed
Lu Dughetti <ludughetti@gmail.com>
2025-07-16 19:26:16 -0300

    Added radius check to node and segment to make sure there are no mesh overlaps

--- Assets/Scripts/Pathfinding/PathNodeGenerator.cs
+++ Assets/Scripts/Pathfinding/PathNodeGenerator.cs
@@ -10,53 +10,64 @@ namespace Pathfinding
         [SerializeField] private Transform lowerLeftLimit;
         [SerializeField] private Transform upperRightLimit;
         [SerializeField, Range(0.1f, 10f)] private float nodesSeparation;
+        [SerializeField, Range(0.1f, 1f)] private float clearanceRadius = 0.5f;
+        [SerializeField, Range(0.1f, 10f)] private float raycastHeight = 3;
         [SerializeField] private string walkableTag;
         [SerializeField] private LayerMask ignoreLayers;
         
         private bool AreNodesAdjacent(PathNode first, PathNode second, float maxSqrDistance)
         {
-            Vector3 diff = first.Position - second.Position;
+            if (first == second) return false;
 
-            return first != second && diff.sqrMagnitude <= maxSqrDistance;
+            // Check distance between nodes
+            var diff = first.Position - second.Position;
+            if (diff.sqrMagnitude > maxSqrDistance) return false;
+
+            // Check if there's a clear line between nodes
+            // to avoid mesh overlap in the middle of a segment
+            var start = first.Position + Vector3.up * raycastHeight;
+            var end = second.Position + Vector3.up * raycastHeight;
+            
+            return !Physics.CheckCapsule(start, end, clearanceRadius, ~ignoreLayers);
         }
 
         public List<PathNode> GenerateNodes()
         {
-            List<PathNode> nodes = new List<PathNode>();
+            var nodes = new List<PathNode>();
 
-            float separationSqr = nodesSeparation * nodesSeparation;
-            float maxSqrDistance = separationSqr + separationSqr;
+            var separationSqr = nodesSeparation * nodesSeparation;
+            var maxSqrDistance = separationSqr + separationSqr;
 
-            const float raycastHeight = 100f;
+            var startX = lowerLeftLimit.position.x;
+            var endX = upperRightLimit.position.x;
+            var startZ = lowerLeftLimit.position.z;
+            var endZ = upperRightLimit.position.z;
 
-            float startX = lowerLeftLimit.position.x;
-            float endX = upperRightLimit.position.x;
-            float startZ = lowerLeftLimit.position.z;
-            float endZ = upperRightLimit.position.z;
-
-            for (float x = startX; x <= endX; x += nodesSeparation)
+            for (var x = startX; x <= endX; x += nodesSeparation)
             {
-                for (float z = startZ; z <= endZ; z += nodesSeparation)
+                for (var z = startZ; z <= endZ; z += nodesSeparation)
                 {
-                    RaycastHit hitInfo;
-                    Vector3 rayOrigin = new Vector3(x, raycastHeight, z);
-
-                    if (Physics.Raycast(rayOrigin, Vector3.down, out hitInfo, raycastHeight, ~ignoreLayers))
+                    var rayOrigin = new Vector3(x, raycastHeight, z);
+                    if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hitInfo, raycastHeight, ~ignoreLayers))
                     {
                         if (hitInfo.collider.CompareTag(walkableTag))
                         {
-                            PathNode pathNode = new PathNode()
+                            // Depth check: ensures there's enough clearance for the agent
+                            if (!Physics.CheckSphere(rayOrigin, clearanceRadius, ~ignoreLayers))
                             {
-                                Position = hitInfo.point
-                            };
+                                PathNode pathNode = new PathNode()
+                                {
+                                    Position = hitInfo.point
+                                };
 
-                            nodes.Add(pathNode);
+                                nodes.Add(pathNode);
+                            }
                         }
                     }
                 }
             }
 
-            foreach (PathNode node in nodes)
+            foreach (var node in nodes)
                 node.AdjacentNodes = nodes.FindAll(n => AreNodesAdjacent(node, n, maxSqrDistance));
 
             return nodes;
